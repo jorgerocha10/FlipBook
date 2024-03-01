c1678301d4ec5af66b442fffeaa7e6e96e00fb7a
Jorge <jorgerocha_10@hotmail.com>
2024-03-01 07:34:45 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template, redirect, url_for
+from azure.storage.blob import BlobServiceClient, BlobClient
+from dotenv import load_dotenv
+import os
+
+
+app = Flask(__name__)
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Now access your variables
+STORAGE_ACCOUNT_URL = os.getenv('AZURE_STORAGE_URL')
+STORAGE_ACCOUNT_KEY = os.environ.get('AZURE_STORAGE_KEY')
+if STORAGE_ACCOUNT_KEY and not STORAGE_ACCOUNT_KEY.endswith('=='):
+    STORAGE_ACCOUNT_KEY += '=='
+CONTAINER_NAME = os.environ.get('CONTAINER_NAME')
+
+# Initialize the BlobServiceClient
+blob_service_client = BlobServiceClient(account_url=STORAGE_ACCOUNT_URL, credential=STORAGE_ACCOUNT_KEY)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file.filename)
+        blob_client.upload_blob(file.read(), overwrite=True)
+        return 'File uploaded successfully!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
